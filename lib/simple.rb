85a6fc066bf53c0b283500484e1146cc0b60bcd7
Paul Springett <paul@springett.me>
2013-04-05 11:42:49 +0100

    Fix namespace madness

--- lib/simple.rb
+++
@@ -1,2 +0,0 @@
-require 'simple/validators/version'
-require 'simple/validators'
--- lib/simple/validators.rb
+++ lib/simple/validators.rb
@@ -1,2 +1,4 @@
+require 'simple/validators/version'
+
 require 'simple/validators/email_validator'
 require 'simple/validators/username_validator'
